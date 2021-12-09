24f70f1819eabeed1779a6c003dfa2de7d0be569
Joshua Flory <joshf98@aol.com>
2021-12-09 16:12:47 -0600

    implemented methods for member controller

--- src/main/java/com/sg/capstone/controller/MemberController.java
+++ src/main/java/com/sg/capstone/controller/MemberController.java
@@ -1,12 +1,20 @@
 package com.sg.capstone.controller;
 
 import com.sg.capstone.model.Post;
+import com.sg.capstone.service.ContentManagementService;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * This controller contains the different endpoints used by a member of the blog
@@ -15,33 +23,43 @@ import java.util.List;
 @RequestMapping("/member")
 public class MemberController {
 
+    @Autowired
+    ContentManagementService service;
     // Gets all the posts from the system
     @GetMapping("/posts")
     public List<Post> getAllPosts() {
-        return null;
+        return service.getAllPosts(false, false, false, null);
     }
 
     // Gets all the unexpired posts from the system
     @GetMapping("/posts/unexpired")
     public List<Post> getAllUnexpiredPosts() {
-        return null;
+        return service.getAllPosts(true, false, false, null);
     }
 
     // Gets all expired posts from the system
     @GetMapping("/posts/expired")
     public List<Post> getAllExpiredPosts() {
-        return null;
+        // Filter out posts that aren't expired
+        return service.getAllPosts(false, true, false, null).stream().filter(post -> post.getExpireDate().isAfter(LocalDateTime.now())).toList();
     }
 
     // Gets a posts from the system by id
     @PostMapping("/posts/{postId}")
-    public Post getPostById() {
-        return null;
+    public Post getPostById(@PathVariable int postId) {
+        return service.getPostById(postId, false, false, false, null);
     }
 
     // Adds a post to the system, but needs approval
     @PostMapping("/addpost")
-    public Post addPostWithPendingApproval() {
-        return null;
+    public Post addPostWithPendingApproval(@RequestBody Post post) {
+        Matcher m = Pattern.compile("(#\\S+)").matcher(post.getContent());
+        List<String> tagList = new ArrayList<String>();
+        while(m.find()) {
+            String find = m.group();
+            if(!tagList.contains(find.toLowerCase())) tagList.add(find);
+        }
+        post.setApproved(false);
+        return service.addPost(post, tagList.toArray(new String[0]));
     }
 }
