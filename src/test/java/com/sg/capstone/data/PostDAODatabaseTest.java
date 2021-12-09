9f8f577d6fe48c6111997537e94949015925e739
Ashley Macote <ashmacote@gmail.com>
2021-12-09 16:58:11 -0500

    adding testing folder

---
+++ src/test/java/com/sg/capstone/data/PostDAODatabaseTest.java
@@ -0,0 +1,41 @@
+package com.sg.capstone.data;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostDAODatabaseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addPost() {
+    }
+
+    @Test
+    void getAllPosts() {
+    }
+
+    @Test
+    void getExpiredPosts() {
+    }
+
+    @Test
+    void getUnexpiredPosts() {
+    }
+
+    @Test
+    void getPostById() {
+    }
+
+    @Test
+    void editPost() {
+    }
+
+    @Test
+    void deletePost() {
+    }
+}
