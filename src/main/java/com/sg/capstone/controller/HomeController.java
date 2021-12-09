efbc8ba6ba3936e7542b77186127c7c6d928ff01
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2021-12-09 15:01:06 -0500

    added comment on the controllers

--- src/main/java/com/sg/capstone/controller/HomeController.java
+++ src/main/java/com/sg/capstone/controller/HomeController.java
@@ -5,15 +5,20 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * This controller contains the different endpoints used by a regular user
+ */
 @RestController
 @RequestMapping("/home")
 public class HomeController {
 
+    // Gets all the valid posts
     @GetMapping("/posts")
     public List<Post> getAllValidPost() {
         return null;
     }
 
+    // Gets a valid post by id
     @GetMapping("/posts/{postId}")
     public Post getValidPostById() {
         return null;
