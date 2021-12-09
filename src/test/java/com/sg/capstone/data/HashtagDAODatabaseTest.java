981ec092dfd1172099b46c1fdf1e394f32a453c4
Ashley Macote <ashmacote@gmail.com>
2021-12-09 17:02:13 -0500

    created testing dao files

---
+++ src/test/java/com/sg/capstone/data/HashtagDAODatabaseTest.java
@@ -0,0 +1,41 @@
+package com.sg.capstone.data;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HashtagDAODatabaseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllHashtags() {
+    }
+
+    @Test
+    void getAllUniqueHashtags() {
+    }
+
+    @Test
+    void getHashtagsByPostId() {
+    }
+
+    @Test
+    void getHashtagListByTag() {
+    }
+
+    @Test
+    void hashtagExists() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
