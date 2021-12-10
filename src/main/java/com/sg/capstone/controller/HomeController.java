e3dbb8e3e54a803a4761b94f200a63bc84984096
Ashley Macote <ashmacote@gmail.com>
2021-12-09 21:14:21 -0500

    Created home controller

--- src/main/java/com/sg/capstone/controller/HomeController.java
+++ src/main/java/com/sg/capstone/controller/HomeController.java
@@ -1,9 +1,11 @@
 package com.sg.capstone.controller;
 
 import com.sg.capstone.model.Post;
+import com.sg.capstone.service.ContentManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import com.sg.capstone.service.ContentManagementService;
 
 /**
  * This controller contains the different endpoints used by a regular user
@@ -12,15 +14,18 @@ import java.util.List;
 @RequestMapping("/home")
 public class HomeController {
 
+    @Autowired
+    ContentManagementService service;
+
     // Gets all the valid posts
     @GetMapping("/posts")
     public List<Post> getAllValidPost() {
-        return null;
+        return service.getAllPosts(false, true, true, null);
     }
 
     // Gets a valid post by id
     @GetMapping("/posts/{postId}")
-    public Post getValidPostById() {
-        return null;
+    public Post getValidPostById(@PathVariable int postId) {
+        return service.getPostById(postId, false, true, true, null);
     }
 }
